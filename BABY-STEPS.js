0242efc60ab95ec19d8554ae74b9be173b594b74
anand <anandp033@gmail.com>
2019-02-13 17:31:00 +0530

    ✍2✍🏼

---
+++ BABY-STEPS.js
@@ -0,0 +1,16 @@
+// ## BABY STEPS(Exercise 2 of 13)
+
+// Write a program that accepts one or more numbers as command - line arguments
+// and prints the sum of those numbers to the console(stdout).
+// for (let i = 2; i < process.argv.length; i++) {
+//   console.log(process.argv[i]);
+
+// }
+function babySteps() {
+  let sum = 0;
+  for (let i = 2; i < process.argv.length; i++) {
+    sum += Number(process.argv[i]);
+  }
+  return sum;
+}
+console.log(babySteps());
